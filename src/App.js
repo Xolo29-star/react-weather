a45dfd1b950b73b1448fb786915be6ab9c128cdb
Lucy Nowland <lucynowland03@gmail.com>
2024-03-21 13:02:28 +0000

    Add link to GitHub repository & Netlify URL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ export default function App() {
     <div className="App">
       <h1>Weather AJAX Search Engine</h1>
       <WeatherSearch />
+      <br />
+      <p>
+        This project was coded by{" "}
+        <a href="https://github.com/lucynowland">Lucy Nowland</a> which is
+        open-sourced on{" "}
+        <a href="https://github.com/lucynowland/react-weather">GitHub</a> and
+        hosted on <a href="https://react-weather-hw.netlify.app/">Netlify</a>
+      </p>
     </div>
   );
 }
