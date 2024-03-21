bb00976ff76c4860dcd9bd6a48b9eaeb82e21aa8
Lucy Nowland <lucynowland03@gmail.com>
2024-03-21 13:03:59 +0000

    Add target and rel attributes to links

--- src/App.js
+++ src/App.js
@@ -9,10 +9,29 @@ export default function App() {
       <br />
       <p>
         This project was coded by{" "}
-        <a href="https://github.com/lucynowland">Lucy Nowland</a> which is
-        open-sourced on{" "}
-        <a href="https://github.com/lucynowland/react-weather">GitHub</a> and
-        hosted on <a href="https://react-weather-hw.netlify.app/">Netlify</a>
+        <a
+          href="https://github.com/lucynowland"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Lucy Nowland
+        </a>{" "}
+        which is open-sourced on{" "}
+        <a
+          href="https://github.com/lucynowland/react-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://react-weather-hw.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
       </p>
     </div>
   );
